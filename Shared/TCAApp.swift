e11cfe02b5e4f04c75b530a781b74e30c4a48f3d
Henry Fan <abcdz330@gmail.com>
2022-05-20 15:00:19 +0800

    Initial Commit

---
+++ Shared/TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAApp.swift
+//  Shared
+//
+//  Created by Henry Fan on 2022/5/20.
+//
+
+import SwiftUI
+
+@main
+struct TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
